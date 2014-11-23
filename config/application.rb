ac129a5fd91dde8d05dc6e85a49d8f6bc9fa5765
mikeyhogarth <mikehogarth20@hotmail.com>
2014-11-23 21:48:31 +0000

    * If a tree falls in the forest and nobody is around

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Picoblog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Add font path to asset pipeline
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
   end
 end
